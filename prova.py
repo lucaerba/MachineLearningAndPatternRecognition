381f75afe05c623477ee252a513637c18e0b6108
lucaerbi <erba.juve@gmail.com>
2023-03-02 17:30:43 +0100

    cazzi miei

--- prova.py
+++ prova.py
@@ -1,1 +1,16 @@
-print ("ciao come va")
+import numpy
+def fun():
+    a = 2
+    print(a)
+    
+def fun2(a=3, b=2):
+    print([a, " " , b])
+
+def fun3(*a, **kw):
+    print(kw)
+    print(a)
+
+l = [[1,'c'],[2,'a'],[3,'b']]
+print(l)
+l = sorted(l, key = lambda x: x[1])
+print(l)
