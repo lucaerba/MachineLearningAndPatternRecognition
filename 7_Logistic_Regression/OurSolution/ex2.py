cfdbac54b53a0a1d7d0dd0cc15db2da7fd89b714
LUCA ERBI s319024 <s319024@studenti.polito.it>
2023-05-05 15:09:38 +0200

    l7 v2

--- 7_Logistic_Regression/OurSolution/ex2.py
+++ 7_Logistic_Regression/OurSolution/ex2.py
@@ -21,5 +21,32 @@ def split_db_2to1(D, L, seed=0):
     LTE = L[idxTest]
     return (DTR, LTR), (DTE, LTE)
 
+def J(w, b, DTR, LTR, l):
+    #first = l/2*np.square(np.linalg.norm(w))
+    #second = np.sum(np.logaddexp(0, -(2*LTR-1)*(np.transpose(w)*DTR+b)))*(1/len(DTR))
+     # Compute the regularizer term
+    reg_term = (l/2) * np.sum(np.power(w, 2))
+
+    # Compute the logistic loss term
+    n = len(DTR)
+    loss_term = 0
+    for i in range(n):
+        loss_term += np.log(1 + np.exp(-(2 * LTR[i] - 1) * (np.dot(np.transpose(DTR)[i], np.transpose(w)) + b) ))
+    loss_term = loss_term*1/n
+    # Compute the full objective function
+    objective = reg_term + loss_term
+    return objective
+
+def logreg_obj(v, DTR, LTR, l):
+    w, b = v[0:-1], v[-1]
+    return J(w, b, DTR, LTR, l)
+
+
 D, L = load_iris_binary()
 (DTR, LTR), (DTE, LTE) = split_db_2to1(D, L)
+print(DTR)
+(x, f, d) = sp.optimize.fmin_l_bfgs_b(logreg_obj, np.zeros(DTR.shape[0] + 1), approx_grad = True, args=(DTR, LTR, 1), iprint=1, maxfun=15000, maxiter=1000)
+print(x)
+print(f)
+print(d)
+
