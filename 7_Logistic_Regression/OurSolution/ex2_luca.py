4f29f400df2823d0505ebdf79fcd0b0028ac01dd
LUCA ERBI s319024 <s319024@studenti.polito.it>
2023-05-05 23:24:17 +0200

    fin luca

--- 7_Logistic_Regression/OurSolution/ex2_luca.py
+++ 7_Logistic_Regression/OurSolution/ex2_luca.py
@@ -24,29 +24,37 @@ def split_db_2to1(D, L, seed=0):
 def J(w, b, DTR, LTR, l):
     #first = l/2*np.square(np.linalg.norm(w))
     #second = np.sum(np.logaddexp(0, -(2*LTR-1)*(np.transpose(w)*DTR+b)))*(1/len(DTR))
-     # Compute the regularizer term
-    reg_term = (l/2) * np.sum(np.power(w, 2))
+     # Compute the regularizer term np.sum(np.power(w, 2))
+    reg_term = (l/2) * np.square(np.linalg.norm(w))
 
     # Compute the logistic loss term
-    n = len(DTR)
+    NEW_DTR = np.transpose(DTR)
+    n = len(NEW_DTR)
     loss_term = 0
     for i in range(n):
-        loss_term += np.log(1 + np.exp(-(2 * LTR[i] - 1) * (np.dot(np.transpose(DTR)[i], np.transpose(w)) + b) ))
+        loss_term += np.logaddexp(0,-(2 * LTR[i] - 1) * (np.dot(NEW_DTR[i], np.transpose(w)) + b))
     loss_term = loss_term*1/n
     # Compute the full objective function
     objective = reg_term + loss_term
     return objective
-
+w_f, b_f = [], 0
 def logreg_obj(v, DTR, LTR, l):
     w, b = v[0:-1], v[-1]
+    w_f, b_f = w, b
     return J(w, b, DTR, LTR, l)
-
-
+#-----------------------------------------#
 D, L = load_iris_binary()
 (DTR, LTR), (DTE, LTE) = split_db_2to1(D, L)
-print(DTR)
-(x, f, d) = sp.optimize.fmin_l_bfgs_b(logreg_obj, np.zeros(DTR.shape[0] + 1), approx_grad = True, args=(DTR, LTR, 1), iprint=1, maxfun=15000, maxiter=1000)
-print(x)
-print(f)
-print(d)
 
+lam = [10**-6, 10**-3, 10**-1, 1]
+for l in lam:
+    (x, f, d) = sp.optimize.fmin_l_bfgs_b(logreg_obj, np.zeros(DTR.shape[0] + 1), approx_grad = True, args=(DTR, LTR, l))
+    print("lam " + str(l) + " min:" + str(f))
+    #print(x,d)
+    S = np.dot(x[0:-1].T, DTE) + x[-1]
+    #print(S)    
+    S_sc = [1 if S[i]>0 else 0 for i in range(len(DTE.T))]
+    #print(S_sc)
+    check = S_sc==LTE
+    print(1-len(check[check==True])/len(LTE))
+    
