fb06920a5d412e4552a1db9f570844a769967544
LUCA ERBI s319024 <s319024@studenti.polito.it>
2023-05-27 16:15:21 +0200

    Job con la j culo

---
+++ 8_Bayes_Decisions_Model_Evaluation/OurSolution/es1.py
@@ -0,0 +1,7 @@
+import sklearn.datasets
+import scipy as sp
+import numpy as np
+
+
+if __name__ == '__main__':
+    
--- Project/src/main.py
+++ Project/src/main.py
@@ -14,10 +14,10 @@ def main():
     D, L = input.load(input.traininput)
 
     model.logreg_kfold_wrapper(D,L)
-    # model.MVG_kfold_wrapper(D, L)
-    # model.NB_kfold_wrapper(D, L)
-    # model.TMVG_kfold_wrapper(D, L)
-    # model.TNB_kfold_wrapper(D, L)
+    model.MVG_kfold_wrapper(D, L)
+    model.NB_kfold_wrapper(D, L)
+    model.TMVG_kfold_wrapper(D, L)
+    model.TNB_kfold_wrapper(D, L)
     
 if __name__ == '__main__':
     main()
